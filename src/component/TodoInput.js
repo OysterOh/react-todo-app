1ac1444be11c1413477de3f0accdaf6ac2a22816
jungwon <jungwon2433@naver.com>
2023-06-13 16:55:15 +0900

    #4 scss 작성 완료

--- src/component/TodoInput.js
+++ src/component/TodoInput.js
@@ -11,7 +11,7 @@ const TodoInput = () => {
             </form>
         </div>
 
-        <button className='insert-btn'>
+        <button className='insert-btn open'>
             <MdAdd />
         </button>
     </>
