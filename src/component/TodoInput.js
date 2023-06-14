d6d4b2568398f58d4fd6714da55bbdd47bd8764c
jungwon <jungwon2433@naver.com>
2023-06-14 15:14:00 +0900

    #6 할 일 추가 기능

--- src/component/TodoInput.js
+++ src/component/TodoInput.js
@@ -3,10 +3,13 @@ import { MdAdd } from "react-icons/md";
 import cn from 'classnames';
 
 import './scss/TodoInput.scss'
-const TodoInput = () => {
+const TodoInput = ({ addTodo }) => {
 
     //입력창이 열리는 여부를 표현하는 상태값
     const [open, setOpen] = useState(false)
+
+    //할 일 입력창에 입력한 내용을 표현하는 상태값
+    const [todoText, setTodoText] = useState('');
 
     // + 버튼 클릭 시 이벤트 처리
     const onToggle = () => {
@@ -15,15 +18,41 @@ const TodoInput = () => {
         
     }
 
+    //submit 이벤트 핸들러
+    const submitHandler = e => {
+        e.preventDefault(); //태그 기본 기능 제한
+        console.log('폼 제출');
+
+        addTodo(todoText);
+
+        //입력이 끝나면 입력창 비우기
+        setTodoText('');
+
+        {/*const $input = document.querySelector('.insert-form input');
+        console.log($input.value);
+
+        $input.value = '';*/}
+    }
+
+    //input change 이벤트 핸들러 함수
+    const todoChangeHandler = e => {
+        //console.log(e.target.value);
+        setTodoText(e.target.value);
+    }
+
     return (
     <>
         {
             open && (
                 <div className='form-wrapper'>
-                <form className='insert-form'>
-                <input type='text' 
-                placeholder='할 일을 입력 후 엔터를 누르세요.'/>
-                </form>
+                    <form className='insert-form' onSubmit={submitHandler}>
+                        <input 
+                            type='text' 
+                            placeholder='할 일을 입력 후 엔터를 누르세요.'
+                            onChange={todoChangeHandler}
+                            value={todoText}
+                        />
+                    </form>
                 </div>)
         }
 
